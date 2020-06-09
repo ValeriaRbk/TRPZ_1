9883839d2e875115f9c33d76618a9025264c091c
ValeriaRbk <vriabikova37@gmail.com>
2020-06-09 15:24:42 +0300

    New 3 classes

---
+++ TRPZ_1/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPZ_1
+{
+    class Car
+    {
+        int distance;
+        int cost;
+        int profit;
+        List<Detail> details = new List<Detail>();
+
+        public Car()
+        {
+
+        }
+
+        public void AddDetail(Detail new_detail) { details.Add(new_detail); }
+    }
+}
---
+++ TRPZ_1/Gamer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPZ_1
+{
+    class Gamer
+    {
+        
+        int money = 100;
+
+        public void MakeACar(Detail detail)
+        {
+            Car car = new Car();
+        }
+
+        public void BuyDetail(int x)
+        {
+            money -= x;
+        }
+
+        public void profit(int x)
+        {
+            money += x;
+        }
+    }
+}
