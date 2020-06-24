b8400132fcb38bdac772c1b560888ad69e09566a
ValeriaRbk <vriabikova37@gmail.com>
2020-06-24 14:03:57 +0300

    mvvm

---
+++ TRPZ_1/Models/Entities/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPZ_1.Models.Entities
+{
+    public class Car 
+    {
+        public List <Detail> Details;
+
+        public int Disance { get; set; }
+
+        public int Profit { get; set; }
+    }
+}
